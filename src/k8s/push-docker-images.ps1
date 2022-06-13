570720c7704d1892f63f5a0557d30e5912821ef6
Lei Xu <leixu@leixu-macpro16>
2022-06-13 12:53:45 +0800

    update script

--- src/k8s/push-docker-images.ps1
+++ src/k8s/push-docker-images.ps1
@@ -1,5 +1,5 @@
-docker push smartide/dapr-trafficcontrol-mosquitto:1.0 
-docker push smartide/dapr-trafficcontrol-trafficcontrolservice:1.0 
+docker push smartide/dapr-trafficcontrol-mosquitto:1.0
+docker push smartide/dapr-trafficcontrol-trafficcontrolservice:1.0
 docker push smartide/dapr-trafficcontrol-finecollectionservice:1.0
-docker push smartide/dapr-trafficcontrol-vehicleregistrationservice:1.0 
+docker push smartide/dapr-trafficcontrol-vehicleregistrationservice:1.0
 docker push smartide/dapr-trafficcontrol-simulation:1.0
